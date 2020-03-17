9a1b74e613dad2ae0d6c28a88f6ae53934cd9a03
Your Name <you@example.com>
2020-03-17 13:29:41 -0600

    Updates

---
+++ precache-manifest.034bab4f2af262611328296e8523f54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fc991a85046fd7a9f95e2aeb7bed38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b0a6c727b601a733fb5",
+    "url": "/static/css/main.50062b1f.chunk.css"
+  },
+  {
+    "revision": "7117515ac2b04a2ab75a",
+    "url": "/static/js/2.c8b29a1a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c8b29a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0a6c727b601a733fb5",
+    "url": "/static/js/main.3e7df317.chunk.js"
+  },
+  {
+    "revision": "3e6d854ee13c0189066f",
+    "url": "/static/js/runtime-main.8faee8ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nadivfolio.space/precache-manifest.8bb5da250af75d1c96d2c88d0ffdd732.js"
+  "/precache-manifest.034bab4f2af262611328296e8523f54e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nadivfolio.space/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
